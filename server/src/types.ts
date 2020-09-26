fc8e23f32f4613fa0532156f1e40665d7b9815d3
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-26 19:39:23 +0530

    Add types for user ID in session storage

--- server/src/types.ts
+++ server/src/types.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session: Express.Session };
+  req: Request & { session: Express.Session & { userId?: number } };
   res: Response;
 };
