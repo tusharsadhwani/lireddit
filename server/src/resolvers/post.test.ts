16229f56eb653269d8e7f13787e7f0f439d79ece
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-30 22:26:45 +0530

    fix tests

--- server/src/resolvers/post.test.ts
+++ server/src/resolvers/post.test.ts
@@ -16,13 +16,11 @@ afterAll(async () => {
 describe("check if database works", () => {
   const postTitle = "Test post";
 
-  it("creates a test post", async () => {
+  it("creates and checks a test post", async () => {
     const post = orm.em.create(Post, { title: postTitle });
     expect(post.title).toEqual(postTitle);
     await orm.em.persistAndFlush(post);
-  });
 
-  it("checks if the post persisted", async () => {
     const fetchedPost = await orm.em.findOne(Post, { title: postTitle });
     expect(fetchedPost?.title).toEqual(postTitle);
   });
@@ -31,7 +29,7 @@ describe("check if database works", () => {
 describe("check if graphql works", () => {
   const postTitle = "Test post 2";
 
-  it("creates a test post using graphql", async () => {
+  it("creates and checks a test post using graphql", async () => {
     await gCall({
       source: `
         mutation createPost($title: String!) {
@@ -49,9 +47,7 @@ describe("check if graphql works", () => {
       variableValues: { title: postTitle },
       contextValue: { em: orm.em, req: {} as any, res: {} as any },
     });
-  });
 
-  it("checks if the post persisted", async () => {
     const fetchedPost = await orm.em.findOne(Post, { title: postTitle });
     expect(fetchedPost?.title).toEqual(postTitle);
   });
