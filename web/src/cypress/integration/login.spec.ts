e53a975090bbba4c27f919bd4fb542ccd86e4326
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-10-01 00:52:01 +0530

    Fix tsconfig

--- web/src/cypress/integration/login.spec.ts
+++ web/src/cypress/integration/login.spec.ts
@@ -1,19 +1,19 @@
-describe("Test login", () => {
-  it("Open login page", () => {
-    cy.visit("http://localhost:3000");
+export const test = () => {
+  describe("Test login", () => {
+    it("Open login page", () => {
+      cy.visit("http://localhost:3000");
 
-    cy.contains("Login").click();
+      cy.contains("Login").click();
+      cy.url().should("include", "/login");
 
-    cy.url().should("include", "/login");
+      cy.get("input#usernameOrEmail").click().type("tushar");
+      cy.get("input#password").click().type("tushar");
+      cy.get("form").contains("Login").last().click();
 
-    cy.get("input#usernameOrEmail").click().type("tushar");
-
-    cy.get("input#password").click().type("tushar");
-
-    cy.get("form").contains("Login").last().click();
-
-    cy.url().should("not.include", "/login");
-
-    cy.get("body").should("contain.text", "Logged in");
+      cy.url().should("not.include", "/login");
+      cy.get("body").should("contain.text", "Logged in");
+    });
   });
-});
+};
+
+test();
