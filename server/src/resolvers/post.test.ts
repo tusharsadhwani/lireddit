f1cd5ae134ae244a355b59c898afb9ee46d04969
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-10-11 23:10:50 +0530

    Add more post tests

--- server/src/resolvers/post.test.ts
+++ server/src/resolvers/post.test.ts
@@ -2,6 +2,7 @@ import { Connection, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
 import { Post } from "../entities/Post";
 import { gCall } from "../test-utils/gCall";
 import { testConnection } from "../test-utils/testConnection";
+import faker from "faker";
 
 let orm: MikroORM<IDatabaseDriver<Connection>>;
 
@@ -14,7 +15,7 @@ afterAll(async () => {
 });
 
 describe("check if database works", () => {
-  const postTitle = "Test post";
+  const postTitle = faker.lorem.sentence();
 
   it("creates and checks a test post", async () => {
     const post = orm.em.create(Post, { title: postTitle });
@@ -27,14 +28,15 @@ describe("check if database works", () => {
 });
 
 describe("check if graphql works", () => {
-  const postTitle = "Test post 2";
+  const postTitle = faker.lorem.sentence();
 
-  it("creates and checks a test post using graphql", async () => {
+  let postId: number | undefined;
+
+  it("create post and check using orm", async () => {
     await gCall({
       source: `
         mutation createPost($title: String!) {
           createPost(title: $title) {
-            error
             post {
               id
               title
@@ -50,5 +52,72 @@ describe("check if graphql works", () => {
 
     const fetchedPost = await orm.em.findOne(Post, { title: postTitle });
     expect(fetchedPost?.title).toEqual(postTitle);
+    postId = fetchedPost?.id;
+  });
+
+  it("fetch posts and check new post is fetched", async () => {
+    const gqlResponse = await gCall({
+      source: `
+        query posts {
+          posts {
+            id
+            title
+            createdAt
+            updatedAt
+          }
+        }
+      `,
+      variableValues: {},
+      contextValue: { em: orm.em, req: {} as any, res: {} as any },
+    });
+
+    expect(
+      (gqlResponse.data?.posts as Post[])
+        .map((post) => post.title)
+        .includes(postTitle)
+    );
+  });
+
+  const updatedPostTitle = faker.lorem.sentence();
+  it("update post", async () => {
+    const gqlResponse = await gCall({
+      source: `
+        mutation updatePost($id: Int!, $title: String!) {
+          updatePost(id: $id, title: $title) {
+            post {
+              id
+              title
+              createdAt
+              updatedAt
+            }
+          }
+        }
+      `,
+      variableValues: { id: postId, title: updatedPostTitle },
+      contextValue: { em: orm.em, req: {} as any, res: {} as any },
+    });
+
+    expect((gqlResponse.data?.updatePost.post as Post).title).toEqual(
+      updatedPostTitle
+    );
+    const fetchedPost = await orm.em.findOne(Post, { id: postId });
+    expect(fetchedPost?.title).toEqual(updatedPostTitle);
+  });
+
+  it("delete post", async () => {
+    const gqlResponse = await gCall({
+      source: `
+        mutation deletePost($id: Int!) {
+          deletePost(id: $id)
+        }
+      `,
+      variableValues: { id: postId },
+      contextValue: { em: orm.em, req: {} as any, res: {} as any },
+    });
+
+    expect(gqlResponse.data?.deletePost).toEqual(true);
+
+    const fetchedPost = await orm.em.findOne(Post, { title: updatedPostTitle });
+    expect(fetchedPost).toBeNull();
   });
 });
